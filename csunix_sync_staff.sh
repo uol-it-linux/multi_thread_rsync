d8f514f1bcb7caa25704f59a3966a4b98b00ec45
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-04 14:06:03 +0100

    Update csunix_sync_staff.sh
    Change staff to user_dirs

--- csunix_sync_staff.sh
+++ csunix_sync_staff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-readarray -t staff_dirs < /root/multi_thread_rsync/staff_directories.txt
+readarray -t user_dirs < /root/multi_thread_rsync/staff_directories.txt
 
 csunix_dir=/mnt/cserv1_a/
 eufs_dir=/uolstore/home/staff_lnxhome01/
 log_file=/tmp/rsync.log
 
-for i in "${staff_dirs[@]}"; do
+for i in "${user_dirs[@]}"; do
   parallel -j 10 --progress --joblog "$log_file" rsync -avz "$csunix_dir/${i}" "$eufs_dir/*" --delete &
 done
 
