391cdced8c6676b8bb9bbe50f5a6ae8940de38f7
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-04 12:40:02 +0100

    remove * from rsync

--- csunix_sync_student.sh
+++ csunix_sync_student.sh
@@ -7,7 +7,7 @@ eufs_dir=/home/dest/
 log_file=/tmp/rsync.log
 
 for i in "${staff_dirs[@]}"; do
-  parallel -j 10 --progress --joblog "$log_file" rsync -avz "$csunix_dir/${i}" "$eufs_dir/*" --delete &
+  parallel -j 10 --progress --joblog "$log_file" rsync -avz "$csunix_dir/${i}" "$eufs_dir/" --delete &
 done
 
 wait
