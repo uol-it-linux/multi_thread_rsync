e20d8362a0bdf6753f58fb8a48460f5b74560283
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-04 14:05:35 +0100

    Update csunix_sync_student.sh
    Set paths and change staff_dirs to user_dirs

--- csunix_sync_student.sh
+++ csunix_sync_student.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-readarray -t staff_dirs < student_directories.txt
+readarray -t user_dirs < /root/multi_thread_rsync/student_directories.txt
 
-csunix_dir=/home/source/
-eufs_dir=/home/dest/
+csunix_dir=/mnt/cserv1_a/
+eufs_dir=/uolstore/home/student_lnxhome01
 log_file=/tmp/rsync.log
 
-for i in "${staff_dirs[@]}"; do
+for i in "${user_dirs[@]}"; do
   parallel -j 10 --progress --joblog "$log_file" rsync -avz "$csunix_dir/${i}" "$eufs_dir/" --delete &
 done
 
