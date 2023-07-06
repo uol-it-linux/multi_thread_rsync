0d4c28bd4ddf8a4e099cc9d8409b7ab9068f2cbf
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-06 13:45:48 +0100

    purge broken scripts, create copy scripts for staff and students

--- copy_csunix_student.sh
+++ copy_csunix_student.sh
@@ -5,11 +5,11 @@ PARALLEL=4
 
 # Set the source and destination paths
 csunix_dir="/mnt/cserv1_a"
-eufs_dir="/uolstore/home/staff_lnxhome01/"
+eufs_dir="/uolstore/home/student_lnxhome01/"
 log_file="/tmp/rsync.log"
 
 # Read the array of user directories from the file
-readarray -t user_dirs < /root/multi_thread_rsync/staff_directories.txt
+readarray -t user_dirs < /root/multi_thread_rsync/student_directories.txt
 
 # Debug test: verify csunix path is correct
 for i in "${!user_dirs[@]}"; do
