cbc6f074274388335736bb4c03b73bbf30f7bef0
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-06 13:06:27 +0100

    More fixing

--- copy_csunix_staff.sh
+++ copy_csunix_staff.sh
@@ -19,7 +19,7 @@ done
 # Loop through the user directories and run rsync in parallel
 for i in "${user_dirs[@]}"; do
   ((i=i%PARALLEL)); ((i++==0)) && wait
-  rsync -avz --exclude-from=/root/rsync-homedir-excludes "$csunix_dir/$i" "$eufs_dir/" --delete &
+  rsync -avz --exclude-from=/root/rsync-homedir-excludes "$csunix_dir/$i/*" "$eufs_dir/" --delete &
 done
 
 # Wait for all rsync processes to finish
