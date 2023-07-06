3200ceb7ed40f6bd48d28a1ca30985a588a19ab1
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-06 12:54:01 +0100

    bug fixing

---
+++ copy_csunix_staff.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the number of parallel processes
+PARALLEL=4
+
+# Set the source and destination paths
+csunix_dir=/mnt/cserv1_a/
+eufs_dir=/uolstore/home/staff_lnxhome01/
+log_file=/tmp/rsync.log
+
+# Read the array of user directories from the file
+readarray -t user_dirs < /root/multi_thread_rsync/staff_directories.txt
+
+# Loop through the user directories and run rsync in parallel
+for i in "${user_dirs[@]}"; do
+  ((i=i%PARALLEL)); ((i++==0)) && wait
+  rsync -avz --exclude-from=/root/rsync-homedir-excludes "$csunix_dir/${i}" "$eufs_dir/" --delete &
+done
+
+# Wait for all rsync processes to finish
+wait
