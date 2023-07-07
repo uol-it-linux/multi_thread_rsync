ffe895a626aa839df84f126cf6b69433eccce9be
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-07-07 14:41:16 +0100

    Update copy_csunix_staff.sh
    Updated line 14 and swapped if to fi line 19 and 22

--- copy_csunix_staff.sh
+++ copy_csunix_staff.sh
@@ -11,15 +11,13 @@ log_file="/tmp/rsync.log"
 # Loop through the user directories and run rsync in parallel
 while IFS="" read -r thisDir || [ -n "$thisDir" ]
 do
-  if [ -d "$csunix_dir/${user_dirs[$i]}" ]; then
-    if [ `ps -efa | grep "rsync.*exclude-from.*rsync-homedir-excludes" | wc -l` < $PARALLEL ]; then
+  if [ -d "$csunix_dir/$thisDir" ]; then
+    if [ $(ps -efa | grep "rsync.*exclude-from.*rsync-homedir-excludes" | wc -l) -lt $PARALLEL ]; then
       rsync -avz --exclude-from=/root/rsync-homedir-excludes -v "$csunix_dir/$thisDir/" "$eufs_dir" --delete >> "$log_file" 2>&1 &
     else
       sleep 10
+    fi
   else
-    echo "Directory $csunix_dir/${user_dirs[$i]} does not exist."
+    echo "Directory $csunix_dir/$thisDir does not exist."
   fi
 done < /root/multi_thread_rsync/staff_directories.txt
-
-
-wait
