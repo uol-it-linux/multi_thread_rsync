a6d43efbbf3d5a7b2fd8fc6bc03b009e4742ffe9
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-08-08 14:04:40 +0100

    fix syntax error in remove_20_symlinks.sh

--- remove_20_symlinks.sh
+++ remove_20_symlinks.sh
@@ -4,7 +4,8 @@ readarray -t user_dirs < /root/multi_thread_rsync/first_twenty_usernames.txt
 
 for i in "${!user_dirs[@]}"; do
 rm /home/csunix/${user_dirs[$i]} 
-fi
 done
+
+for i in 
 
 /bin/bash /root/multi_thread_rsync/create_20_symlinks.sh
