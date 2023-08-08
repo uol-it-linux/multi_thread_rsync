1de3adf3014d528047ba094a94fcb6fadd27f4b2
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-08-08 11:42:21 +0100

    fix log file

--- create_20_symlinks.sh
+++ create_20_symlinks.sh
@@ -6,7 +6,7 @@ readarray -t user_dirs < /root/multi_thread_rsync/first_twenty_usernames.txt
 # Loop through the user directories and create symbolic links
 for i in "${!user_dirs[@]}"; do
   if [ -d "/export/cserv1_a/soc_staff/${user_dirs[$i]}" ]; then
-  echo -e 'ln -s "/export/cserv1_a/soc_staff/${user_dirs[$i]}" "/mnt/eufs003/staff_lnxhome01/${user_dirs[$i]}' >> /root/symlink.log
+  echo -e ln -s "/export/cserv1_a/soc_staff/${user_dirs[$i]}" "/mnt/eufs003/staff_lnxhome01/${user_dirs[$i]}" >> /root/symlink.log
     ln -s "/export/cserv1_a/soc_staff/${user_dirs[$i]}" "/mnt/eufs003/staff_lnxhome01/${user_dirs[$i]}"
   else
     echo "Directory /export/cserv1_a/soc_staff/${user_dirs[$i]} does not exist."
