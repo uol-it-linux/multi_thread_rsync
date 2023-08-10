aa2b6a189649f593ac380a5f8096fd87de7069f5
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-08-10 13:53:48 +0100

    create scripts for switching staff over

---
+++ create_staff_symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Read the array of usernames from the file
+readarray -t user_dirs < /root/multi_thread_rsync/staff_usernames.txt
+
+# Loop through the user directories and create symbolic links
+for i in "${!user_dirs[@]}"; do
+  if [ -d "/export/cserv1_a/soc_staff/${user_dirs[$i]}" ]; then
+  echo -e ln -s "/uolstore/home/staff_lnxhome01/${user_dirs[$i]}" "/home/csunix/${user_dirs[$i]}" >> /root/symlink.log
+    ln -s "/uolstore/home/staff_lnxhome01/${user_dirs[$i]}" "/home/csunix/${user_dirs[$i]}"
+  else
+    echo "Directory /export/cserv1_a/soc_staff/${user_dirs[$i]} does not exist."
+  fi
+done
