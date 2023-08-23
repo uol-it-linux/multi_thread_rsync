e17bf56e0b1ab71bfbd7cf58e7136bcd4e8b9a1f
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-08-23 22:00:35 +0100

    fix mistake of not searching for symlinks on other staff dirs

--- create_staff_symlinks.sh
+++ create_staff_symlinks.sh
@@ -3,12 +3,20 @@
 # Read the array of usernames from the file
 readarray -t user_dirs < /root/multi_thread_rsync/staff_usernames.txt
 
+# Read the array of target directories from another file
+readarray -t target_dirs < /root/multi_thread_rsync/staff_directories.txt
+
+# Redirect all script output to a log file
+exec >> /root/symlink.log 2>&1
+
 # Loop through the user directories and create symbolic links
 for i in "${!user_dirs[@]}"; do
-  if [ -d "/export/cserv1_a/soc_staff/${user_dirs[$i]}" ]; then
-  echo -e ln -s "/uolstore/home/staff_lnxhome01/${user_dirs[$i]}" "/home/csunix/${user_dirs[$i]}" >> /root/symlink.log
-    ln -s "/uolstore/home/staff_lnxhome01/${user_dirs[$i]}" "/home/csunix/${user_dirs[$i]}"
-  else
-    echo "Directory /export/cserv1_a/soc_staff/${user_dirs[$i]} does not exist."
-  fi
+  for dir in "${target_dirs[@]}"; do
+    full_path="/export/cserv1_a/${dir}/${user_dirs[$i]}"
+    if [ -d "$full_path" ]; then
+      echo -e ln -s "$full_path" "/home/csunix/${user_dirs[$i]}" >> /root/symlink.log
+      ln -s "$full_path" "/home/csunix/${user_dirs[$i]}"
+      break  # No need to check other target directories for this user
+    fi
+  done
 done
