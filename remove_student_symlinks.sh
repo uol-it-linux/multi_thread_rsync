9b0690f4e879a60e6cab53a728ec3b0ac5052478
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-09-13 11:45:36 +0100

    change staff to student

--- remove_student_symlinks.sh
+++ remove_student_symlinks.sh
@@ -1,9 +1,11 @@
+#### student_username.txt contains msc students. Consider regenerating it! ####
+
 #!/bin/sh
 
-readarray -t user_dirs < /root/multi_thread_rsync/staff_usernames.txt
+readarray -t user_dirs < /root/multi_thread_rsync/student_usernames.txt
 
 for i in "${!user_dirs[@]}"; do
 rm /home/csunix/${user_dirs[$i]} 
 done
 
-/bin/bash /root/multi_thread_rsync/create_staff_symlinks.sh
+/bin/bash /root/multi_thread_rsync/create_student_symlinks.sh
